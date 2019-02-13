75999dcb90a93cc379dd22f012111cd3af8cc39a
ruffsl <roxfoxpox@gmail.com>
2019-02-13 03:24:25 -0800

    Add demo main

---
+++ bbr_sawtooth_bridge/src/bbr_sawtooth_bridge/demo_main.cpp
@@ -0,0 +1,24 @@
+// Copyright 2019, Ruffin White.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <inttypes.h>
+#include <memory>
+#include "bbr_sawtooth_bridge/bridge_signer.hpp"
+
+int main(int argc, char * argv[])
+{
+  auto foo = bbr_sawtooth_bridge::Signer();
+  (void) foo;
+  return 0;
+}
