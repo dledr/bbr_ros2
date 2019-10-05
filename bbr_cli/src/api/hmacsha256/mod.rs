918a4fcaefb26dc3da7cf683d6d1628fd78c4e18
ruffsl <roxfoxpox@gmail.com>
2019-10-05 16:42:04 -0700

    Linking hashing via hmac

---
+++ bbr_cli/src/api/hmacsha256/mod.rs
@@ -0,0 +1,45 @@
+extern crate sodiumoxide;
+
+use sodiumoxide::crypto::auth::hmacsha256;
+// use utils::byte_array::_clone_into_array;
+
+// TODO: FIXME: I don't like how we convert slices to array.
+// TODO: FIXME: Size checking and keys validation.
+pub fn _clone_into_array<A, T>(slice: &[T]) -> A
+    where A: Sized + Default + AsMut<[T]>, T: Clone
+{
+    let mut a = Default::default();
+    <A as AsMut<[T]>>::as_mut(&mut a).clone_from_slice(slice);
+    a
+}
+
+
+pub struct HMACSHA256Key {
+    key: hmacsha256::Key,
+}
+
+impl HMACSHA256Key {
+    pub fn get_bytes(&self) -> &[u8] {
+        &self.key.0
+    }
+}
+
+
+pub struct HMACSHA256 {}
+
+impl HMACSHA256 {
+    pub const TAGBYTES: usize = hmacsha256::TAGBYTES;
+
+    pub fn generate_key() -> HMACSHA256Key {
+        HMACSHA256Key { key: hmacsha256::gen_key() }
+    }
+
+    pub fn clone_key_from_slice(bytes: &[u8]) -> HMACSHA256Key {
+        HMACSHA256Key { key: hmacsha256::Key(_clone_into_array(bytes)) }
+    }
+
+    pub fn create_tag(data: &[u8], key: &HMACSHA256Key) -> [u8; hmacsha256::TAGBYTES]{
+        let hmacsha256::Tag(tag) = hmacsha256::authenticate(data, &key.key);
+        tag
+    }
+}
--- bbr_cli/src/api/mod.rs
+++ bbr_cli/src/api/mod.rs
@@ -1,29 +1,40 @@
+// extern crate hmac;
+extern crate rusqlite;
+// extern crate sha2;
+
+// use hmac::{Hmac, Mac};
+// use sha2::Sha256;
+
 use std::path::PathBuf;
 
-extern crate rusqlite;
 use rusqlite::NO_PARAMS;
 // use rusqlite::types::ToSql;
 use rusqlite::{params, Connection, Result};
 
+mod hmacsha256;
+use hmacsha256::HMACSHA256;
+
+// type HmacSha256 = Hmac<Sha256>;
+
 pub fn convert(input: PathBuf) -> Result<()> {
-    let mut conn = Connection::open(input)?;
-    let tx = conn.transaction()?;
-    tx.execute("
-        ALTER TABLE topics ADD COLUMN
-            bbr_nonce BLOB NOT NULL DEFAULT
-            x'0000000000000000000000000000000000000000000000000000000000000000';",
-        NO_PARAMS)?;
-    tx.execute("
-        ALTER TABLE topics ADD COLUMN
-            bbr_digest BLOB NOT NULL DEFAULT
-            x'0000000000000000000000000000000000000000000000000000000000000000';",
-        NO_PARAMS)?;
-    tx.execute("
-        ALTER TABLE messages ADD COLUMN
-            bbr_digest BLOB NOT NULL DEFAULT
-            x'0000000000000000000000000000000000000000000000000000000000000000';",
-        NO_PARAMS)?;
-    tx.commit()?;
+    let mut conn = Connection::open(&input)?;
+    // let tx = conn.transaction()?;
+    // tx.execute("
+    //     ALTER TABLE topics ADD COLUMN
+    //         bbr_nonce BLOB NOT NULL DEFAULT
+    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
+    //     NO_PARAMS)?;
+    // tx.execute("
+    //     ALTER TABLE topics ADD COLUMN
+    //         bbr_digest BLOB NOT NULL DEFAULT
+    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
+    //     NO_PARAMS)?;
+    // tx.execute("
+    //     ALTER TABLE messages ADD COLUMN
+    //         bbr_digest BLOB NOT NULL DEFAULT
+    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
+    //     NO_PARAMS)?;
+    // tx.commit()?;
 
     #[derive(Debug)]
     struct Message {
@@ -45,9 +56,31 @@ pub fn convert(input: PathBuf) -> Result<()> {
         })
     })?;
 
+    let mut _conn = Connection::open(&input)?;
+    let tx = _conn.transaction()?;
+
+    let mut hmac_key = HMACSHA256::generate_key();
     for message in messages_iter {
-        println!("Found message {:?}", message.unwrap());
+        let mut msg = message.unwrap();
+        let tag = HMACSHA256::create_tag(
+            &msg.data.unwrap().as_slice(),
+            &hmac_key);
+        // mac.input(msg.data.unwrap().as_slice());
+        // let result = mac.result();
+        msg.bbr_digest = Some(tag.to_vec());
+        println!("Found message {:?}", msg.bbr_digest);
+        // mac.reset();
+        // hmac_key = HMACSHA256Key::from_slice(&tag)
+        tx.execute("
+            UPDATE messages SET 
+                bbr_digest = (?1)
+            WHERE id = (?2)",
+        params![msg.bbr_digest, msg.id],
+        )?;
+        hmac_key = HMACSHA256::clone_key_from_slice(&tag);
     }
+    tx.commit()?;
+
     println!("Done");
     Ok(())
 }
