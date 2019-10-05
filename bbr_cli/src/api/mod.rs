6fcbe25b7832129736896c35f02d5734d5a93799
ruffsl <roxfoxpox@gmail.com>
2019-10-05 13:31:24 -0700

    Add example select iterater

--- bbr_cli/src/api/mod.rs
+++ bbr_cli/src/api/mod.rs
@@ -23,5 +23,31 @@ pub fn convert(input: PathBuf) -> Result<()> {
             bbr_digest BLOB NOT NULL DEFAULT
             x'0000000000000000000000000000000000000000000000000000000000000000';",
         NO_PARAMS)?;
-    tx.commit()
+    tx.commit()?;
+
+    #[derive(Debug)]
+    struct Message {
+        id: i64,
+        topic_id: i64,
+        timestamp: i64,
+        data: Option<Vec<u8>>,
+        bbr_digest: Option<Vec<u8>>,
+    }
+
+    let mut stmt = conn.prepare("SELECT id, topic_id, timestamp, data, bbr_digest FROM messages")?;
+    let messages_iter = stmt.query_map(params![], |row| {
+        Ok(Message {
+            id: row.get(0)?,
+            topic_id: row.get(1)?,
+            timestamp: row.get(2)?,
+            data: row.get(3)?,
+            bbr_digest: row.get(4)?,
+        })
+    })?;
+
+    for message in messages_iter {
+        println!("Found message {:?}", message.unwrap());
+    }
+    println!("Done");
+    Ok(())
 }
