9bac109996b4e87ae39e1c826cab324a1b95813a
ruffsl <roxfoxpox@gmail.com>
2019-10-05 13:30:25 -0700

    Set colum default as 256bit of hex zeros

--- bbr_cli/src/api/mod.rs
+++ bbr_cli/src/api/mod.rs
@@ -1,24 +1,27 @@
 use std::path::PathBuf;
 
 extern crate rusqlite;
-use rusqlite::{Connection, Result};
 use rusqlite::NO_PARAMS;
+// use rusqlite::types::ToSql;
+use rusqlite::{params, Connection, Result};
 
 pub fn convert(input: PathBuf) -> Result<()> {
     let mut conn = Connection::open(input)?;
     let tx = conn.transaction()?;
-
     tx.execute("
         ALTER TABLE topics ADD COLUMN
-            bbr_nonce BLOB NOT NULL DEFAULT 0;",
+            bbr_nonce BLOB NOT NULL DEFAULT
+            x'0000000000000000000000000000000000000000000000000000000000000000';",
         NO_PARAMS)?;
     tx.execute("
         ALTER TABLE topics ADD COLUMN
-            bbr_digest BLOB NOT NULL DEFAULT 0;",
+            bbr_digest BLOB NOT NULL DEFAULT
+            x'0000000000000000000000000000000000000000000000000000000000000000';",
         NO_PARAMS)?;
     tx.execute("
         ALTER TABLE messages ADD COLUMN
-            bbr_digest BLOB NOT NULL DEFAULT 0;",
+            bbr_digest BLOB NOT NULL DEFAULT
+            x'0000000000000000000000000000000000000000000000000000000000000000';",
         NO_PARAMS)?;
     tx.commit()
 }
