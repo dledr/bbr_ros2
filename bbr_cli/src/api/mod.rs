299358c26b10d9fdb6f2a844216d6f262c022e06
ruffsl <roxfoxpox@gmail.com>
2019-10-05 21:50:52 -0700

    Checkpointing topics schema

--- bbr_cli/src/api/mod.rs
+++ bbr_cli/src/api/mod.rs
@@ -1,9 +1,7 @@
-// extern crate hmac;
+extern crate chrono;
 extern crate rusqlite;
-// extern crate sha2;
 
-// use hmac::{Hmac, Mac};
-// use sha2::Sha256;
+use chrono::prelude::Utc;
 
 use std::path::PathBuf;
 
@@ -14,68 +12,141 @@ use rusqlite::{params, Connection, Result};
 mod hmacsha256;
 use hmacsha256::HMACSHA256;
 
-// type HmacSha256 = Hmac<Sha256>;
+#[derive(Debug)]
+struct Message {
+    id: i64,
+    topic_id: i64,
+    timestamp: i64,
+    data: Option<Vec<u8>>,
+    bbr_digest: Option<Vec<u8>>,
+}
+
+#[derive(Debug)]
+struct Topic {
+    id: i64,
+    name: String,
+    serialization_type: String,
+    serialization_format: String,
+    bbr_nonce: Option<Vec<u8>>,
+    bbr_digest: Option<Vec<u8>>,
+}
+
+#[derive(Debug)]
+struct Meta {
+    id: i64,
+    name: String,
+    timestamp: i64,
+    bbr_nonce: Option<Vec<u8>>,
+    bbr_digest: Option<Vec<u8>>,
+}
+
+pub fn alter_tables(conn: &mut Connection) -> Result<()> {
+    let tx = conn.transaction()?;
+    tx.execute("
+        ALTER TABLE topics ADD COLUMN
+            bbr_nonce BLOB NOT NULL DEFAULT
+            x'0000000000000000000000000000000000000000000000000000000000000000';",
+        NO_PARAMS)?;
+    tx.execute("
+        ALTER TABLE topics ADD COLUMN
+            bbr_digest BLOB NOT NULL DEFAULT
+            x'0000000000000000000000000000000000000000000000000000000000000000';",
+        NO_PARAMS)?;
+    tx.commit()
+}
+
+fn create_meta(conn: &mut Connection, meta: &Meta) -> Result<()> {
+    let tx = conn.transaction()?;
+    tx.execute("
+        CREATE TABLE metas (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            timestamp INTEGER NOT NULL,
+            bbr_nonce BLOB NOT NULL,
+            bbr_digest BLOB NOT NULL)",
+        NO_PARAMS)?;
+    tx.execute("
+        INSERT INTO metas (
+            name,
+            timestamp,
+            bbr_nonce,
+            bbr_digest)
+        VALUES (?1, ?2, ?3, ?4)",
+        params![
+            meta.name,
+            meta.timestamp,
+            meta.bbr_nonce,
+            meta.bbr_digest])?;
+    tx.commit()
+}
+
+pub fn get_unix_timestamp_us() -> i64 {
+    let now = Utc::now();
+    now.timestamp_nanos() as i64
+}
 
 pub fn convert(input: PathBuf) -> Result<()> {
     let mut conn = Connection::open(&input)?;
-    // let tx = conn.transaction()?;
-    // tx.execute("
-    //     ALTER TABLE topics ADD COLUMN
-    //         bbr_nonce BLOB NOT NULL DEFAULT
-    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
-    //     NO_PARAMS)?;
-    // tx.execute("
-    //     ALTER TABLE topics ADD COLUMN
-    //         bbr_digest BLOB NOT NULL DEFAULT
-    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
-    //     NO_PARAMS)?;
-    // tx.execute("
-    //     ALTER TABLE messages ADD COLUMN
-    //         bbr_digest BLOB NOT NULL DEFAULT
-    //         x'0000000000000000000000000000000000000000000000000000000000000000';",
-    //     NO_PARAMS)?;
-    // tx.commit()?;
 
-    #[derive(Debug)]
-    struct Message {
-        id: i64,
-        topic_id: i64,
-        timestamp: i64,
-        data: Option<Vec<u8>>,
-        bbr_digest: Option<Vec<u8>>,
-    }
+    let mut meta = Meta {
+        id: 0,
+        name: String::from(
+            input.file_stem().unwrap()
+            .to_str().unwrap()),
+        timestamp: get_unix_timestamp_us(),
+        bbr_nonce: Some(
+            HMACSHA256::generate_key()
+                .get_bytes()
+                .to_vec()),
+        bbr_digest: None,
+    };
 
-    let mut stmt = conn.prepare("SELECT id, topic_id, timestamp, data, bbr_digest FROM messages")?;
-    let messages_iter = stmt.query_map(params![], |row| {
-        Ok(Message {
+    // FIXME: compute digest from name and timestamp
+    meta.bbr_digest = meta.bbr_nonce.clone();
+
+    // create_meta(&mut conn)?;
+    create_meta(&mut conn, &meta)?;
+    alter_tables(&mut conn)?;
+
+    let mut topics_stmt = conn.prepare("
+        SELECT
+            id,
+            name,
+            type,
+            serialization_format
+        FROM topics")?;
+
+    let topics_iter = topics_stmt.query_map(params![], |row| {
+        Ok(Topic {
             id: row.get(0)?,
-            topic_id: row.get(1)?,
-            timestamp: row.get(2)?,
-            data: row.get(3)?,
-            bbr_digest: row.get(4)?,
+            name: row.get(1)?,
+            serialization_type: row.get(2)?,
+            serialization_format: row.get(3)?,
+            bbr_nonce: None,
+            bbr_digest: None,
         })
     })?;
 
     let mut _conn = Connection::open(&input)?;
     let tx = _conn.transaction()?;
 
-    let mut hmac_key = HMACSHA256::generate_key();
-    for message in messages_iter {
-        let mut msg = message.unwrap();
+    let mut hmac_key = HMACSHA256::clone_key_from_slice(
+        meta.bbr_digest.unwrap().as_slice());
+    // let mut hmac_key = HMACSHA256::generate_key();
+    for topic_result in topics_iter {
+        let mut topic = topic_result.unwrap();
         let tag = HMACSHA256::create_tag(
-            &msg.data.unwrap().as_slice(),
+            &topic.name.as_bytes(),
             &hmac_key);
-        // mac.input(msg.data.unwrap().as_slice());
-        // let result = mac.result();
-        msg.bbr_digest = Some(tag.to_vec());
-        println!("Found message {:?}", msg.bbr_digest);
-        // mac.reset();
-        // hmac_key = HMACSHA256Key::from_slice(&tag)
+        topic.bbr_nonce = Some(hmac_key.get_bytes().to_vec());
+        topic.bbr_digest = Some(tag.to_vec());
+        println!("Found topic {:?}", &topic.name);
         tx.execute("
-            UPDATE messages SET 
-                bbr_digest = (?1)
-            WHERE id = (?2)",
-        params![msg.bbr_digest, msg.id],
+            UPDATE topics SET 
+                bbr_nonce = (?1),
+                bbr_digest = (?2)
+            WHERE id = (?3)",
+        params![topic.bbr_nonce, topic.bbr_digest, topic.id],
         )?;
         hmac_key = HMACSHA256::clone_key_from_slice(&tag);
     }
