d36d1158a900d9cb1ff5cd40ba87ae9d00a39632
ruffsl <roxfoxpox@gmail.com>
2019-10-07 23:49:17 -0700

    Add message form struct

--- bbr_cli/src/models/message.rs
+++ bbr_cli/src/models/message.rs
@@ -1,8 +1,17 @@
+use crate::schema::messages;
+
 #[derive(Debug, diesel::Queryable)]
 pub struct Message {
     pub id: i64,
     pub topic_id: i64,
     pub timestamp: i64,
     pub data: Vec<u8>,
+    pub bbr_digest: Option<Vec<u8>>,
+}
+
+#[derive(diesel::AsChangeset, diesel::Identifiable)]
+#[table_name = "messages"]
+pub struct MessageForm {
+    pub id: i64,
     pub bbr_digest: Option<Vec<u8>>,
 }
